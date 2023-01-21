3f91ce25513e4289d4e54d9377b87fb59a327481
Shibin C Reji <scr2404@gmail.com>
2023-01-21 23:24:44 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");1111
+const User = require("../models/user");
 const { check, validationResult } = require("express-validator");
 var jwt = require("jsonwebtoken");
 var expressJwt = require("express-jwt");
