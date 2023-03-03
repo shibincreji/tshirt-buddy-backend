4c107cd1dab18eb9cfb2a592409aeb733ec4fdaa
Shibin C Reji <scr2404@gmail.com>
2023-03-03 12:57:56 +0530

    Update order.js

--- controllers/order.js
+++ controllers/order.js
@@ -1,4 +1,4 @@
-const { Order, ProductCart } = require("../models/order");1
+const { Order, ProductCart } = require("../models/order");11
 
 exports.getOrderById = (req, res, next, id) => {
   Order.findById(id)
