1a55e83b245a277ba2fc83628f1ac75a9e5728c7
Shibin C Reji <scr2404@gmail.com>
2023-08-22 10:21:10 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-1111111var express = require("express");
+11111111var express = require("express");
 var router = express.Router();
 const { check, validationResult } = require("express-validator");
 const { signout, signup, signin, isSignedIn } = require("../controllers/auth");
