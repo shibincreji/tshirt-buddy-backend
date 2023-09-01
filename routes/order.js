0ca0d33a09758468db957ccfa6c726336f01be52
Shibin C Reji <scr2404@gmail.com>
2023-09-01 10:54:01 +0530

    Update order.js

--- routes/order.js
+++ routes/order.js
@@ -1,4 +1,4 @@
-11111const express = require("express");
+111111const express = require("express");
 const router = express.Router();
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
